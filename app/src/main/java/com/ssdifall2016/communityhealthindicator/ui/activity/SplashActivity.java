27b01b9fae665e5c74ff8a472cd89935e901f935
Visesh <viseshrprasad@gmail.com>
2016-11-19 21:59:48 -0500

    Add Splash screen

---
+++ app/src/main/java/com/ssdifall2016/communityhealthindicator/ui/activity/SplashActivity.java
@@ -0,0 +1,59 @@
+package com.ssdifall2016.communityhealthindicator.ui.activity;
+
+import android.content.Intent;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.ssdifall2016.communityhealthindicator.CHIApp;
+import com.ssdifall2016.communityhealthindicator.R;
+import com.viksaa.sssplash.lib.activity.AwesomeSplash;
+import com.viksaa.sssplash.lib.cnst.Flags;
+import com.viksaa.sssplash.lib.model.ConfigSplash;
+
+/**
+ * Created by viseshprasad on 11/19/16.
+ */
+
+public class SplashActivity extends AwesomeSplash {
+    @Override
+    public void initSplash(ConfigSplash configSplash) {
+        //Customize Circular Reveal
+        configSplash.setBackgroundColor(R.color.colorPrimary); //any color you want form colors.xml
+        configSplash.setAnimCircularRevealDuration(2000); //int ms
+        configSplash.setRevealFlagX(Flags.REVEAL_RIGHT);  //or Flags.REVEAL_LEFT
+        configSplash.setRevealFlagY(Flags.REVEAL_BOTTOM); //or Flags.REVEAL_TOP
+
+        //Choose LOGO OR PATH; if you don't provide String value for path it's logo by default
+
+        //Customize Logo
+        configSplash.setLogoSplash(R.drawable.icon); //or any other drawable
+        configSplash.setAnimLogoSplashDuration(2000); //int ms
+        configSplash.setAnimLogoSplashTechnique(Techniques.Bounce); //choose one form Techniques (ref: https://github.com/daimajia/AndroidViewAnimations)
+
+
+        //Customize Path
+        //configSplash.setPathSplash(Constants.DROID_LOGO); //set path String
+        configSplash.setOriginalHeight(400); //in relation to your svg (path) resource
+        configSplash.setOriginalWidth(400); //in relation to your svg (path) resource
+        configSplash.setAnimPathStrokeDrawingDuration(3000);
+        configSplash.setPathSplashStrokeSize(3); //I advise value be <5
+        configSplash.setPathSplashStrokeColor(R.color.colorAccent); //any color you want form colors.xml
+        configSplash.setAnimPathFillingDuration(3000);
+        configSplash.setPathSplashFillColor(R.color.materialRed); //path object filling color
+
+
+        //Customize Title
+        configSplash.setTitleSplash(CHIApp.get().getString(R.string.app_name));
+        configSplash.setTitleTextColor(R.color.colorAccent);
+        configSplash.setTitleTextSize(30f); //float value
+        configSplash.setAnimTitleDuration(3000);
+        configSplash.setAnimTitleTechnique(Techniques.FlipInX);
+        //configSplash.setTitleFont("fonts/myfont.ttf"); //provide string to your font located in assets/fonts/
+
+    }
+
+    @Override
+    public void animationsFinished() {
+        startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+        finish();
+    }
+}
