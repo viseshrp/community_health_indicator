58908cc0242fb138c900df67d4fbacb4d05906d6
Visesh <viseshrprasad@gmail.com>
2016-11-23 22:40:29 -0500

    Fix fragment display

--- app/src/main/java/com/ssdifall2016/communityhealthindicator/ui/activity/MainActivity.java
+++ app/src/main/java/com/ssdifall2016/communityhealthindicator/ui/activity/MainActivity.java
@@ -20,25 +20,16 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.ssdifall2016.communityhealthindicator.R;
+import com.ssdifall2016.communityhealthindicator.ui.fragments.CountyFragment;
+import com.ssdifall2016.communityhealthindicator.ui.fragments.DiseaseFragment;
 import com.ssdifall2016.communityhealthindicator.utils.AppConstants;
 import com.ssdifall2016.communityhealthindicator.utils.MsgUtils;
 import com.ssdifall2016.communityhealthindicator.utils.PreferencesUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
     private ViewPager mViewPager;
 
     private ProgressDialog progressDialog;
@@ -97,52 +88,6 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-
-            if (getArguments().getInt(ARG_SECTION_NUMBER) == 1) {
-                return inflater.inflate(R.layout.fragment_county, container, false);
-            } else if (getArguments().getInt(ARG_SECTION_NUMBER) == 2) {
-                return inflater.inflate(R.layout.fragment_disease, container, false);
-            } else {
-                View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-                TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-                textView.setText(getString(R.string.message_welcome) + " " + PreferencesUtils.getString(getActivity(), AppConstants.USER_FIRST_NAME, "Sir!"));
-                return rootView;
-            }
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -151,9 +96,15 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+
+            switch (position) {
+                case 0:
+                    return CountyFragment.newInstance();
+                case 1:
+                    return DiseaseFragment.newInstance();
+                default:
+                    return null;
+            }
         }
 
         @Override
